703a0563124bf708cfda1f787668c9103f16ce0e
manvitha <jsmr486@gmail.com>
2025-01-31 09:56:51 +0530

    sfsa

---
+++ frontend/src/Path/loginpage.jsx
@@ -0,0 +1,10 @@
+import Login from "./login";
+import React from 'react'
+const LoginPage =()=>{
+    return(
+        <div>
+            <Login/>
+        </div>
+    )
+}
+export default LoginPage
